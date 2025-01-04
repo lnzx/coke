7b438a81f4ab7b15c55fff4cb33b0afc28af119d
lnzx <lnx@tutamail.com>
2025-01-04 14:58:56 +0800

    update search input

--- web/coke/src/views/DashView.vue
+++ web/coke/src/views/DashView.vue
@@ -72,7 +72,7 @@ onMounted(() => {
         <form class="ml-auto flex-1 sm:flex-initial">
           <div class="relative">
             <Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input type="search" placeholder="Search products..." class="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]" />
+            <Input type="search" placeholder="Search machines..." class="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]" />
           </div>
         </form>
         <DropdownMenu>
